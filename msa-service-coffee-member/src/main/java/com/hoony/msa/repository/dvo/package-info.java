bc932c10c0573ba8088c9af05d221afd61e9ca69
leejm1 <leejm1@gbnet.kr>
2019-08-14 17:46:35 +0900

    package rename

--- msa-service-coffee-member/src/main/java/com/hoony/msa/repository/dvo/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.hoony.msa.repository.dvo;
--- msa-service-coffee-member/src/main/java/com/leejm/msa/MicroServiceApplication.java
+++ msa-service-coffee-member/src/main/java/com/leejm/msa/MicroServiceApplication.java
@@ -1,4 +1,4 @@
-package com.hoony.msa;
+package com.leejm.msa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
